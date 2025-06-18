b82aee9e1e1f69b83f6db66c85f149328f0ee83f
JMaxRowe <jmaxrowe@gmail.com>
2025-06-18 13:08:22 +0100

    first three exercises done

---
+++ app.js
@@ -0,0 +1,30 @@
+const foods = [];
+
+console.log("Exercise 1 result:", foods);
+/*
+Exercise 2: Add strings to the array
+
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+foods.push("pizza")
+foods.push("cheeseburger")
+
+
+console.log('Exercise 2 result:', foods);
+
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+foods.unshift("taco")
+
+
+console.log('Exercise 3 result:', foods);
+
